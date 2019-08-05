ce2ea1327e2b515313652c39068566339872b7d4
Prof-Bloodstone <46570876+Prof-Bloodstone@users.noreply.github.com>
2019-08-05 04:29:37 +0200

    Modularize backup providers and add restic as a backup provider (#3)

--- backup-loop.sh
+++ backup-loop.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-readonly backup_extension="tgz"
 : "${SRC_DIR:=/data}"
 : "${DEST_DIR:=/backups}"
 : "${BACKUP_NAME:=world}"
 : "${INITIAL_DELAY:=2m}"
 : "${BACKUP_INTERVAL:=${INTERVAL_SEC:-24h}}"
+: "${BACKUP_METHOD:=tar}" # currently one of tar, restic
 : "${PRUNE_BACKUPS_DAYS:=7}"
 : "${RCON_PORT:=25575}"
 : "${RCON_PASSWORD:=minecraft}"
@@ -17,13 +17,45 @@ readonly backup_extension="tgz"
 export RCON_PORT
 export RCON_PASSWORD
 
+###############
+##  common   ##
+## functions ##
+###############
+
+is_elem_in_array() {
+  # $1 = element
+  # All remaining arguments are array to search for the element in
+  if [ "$#" -lt 2 ]; then
+    log INTERNALERROR "Wrong number of arguments passed to is_elem_in_array function"
+    return 2
+  fi
+  local element="${1}"
+  shift
+  local e
+  for e; do
+    if [ "${element}" == "${e}" ]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
 log() {
   if [ "$#" -lt 1 ]; then
-    echo "Wrong number of arguments passed to log function" >&2
-    return 1
+    log INTERNALERROR "Wrong number of arguments passed to log function"
+    return 2
   fi
   local level="${1}"
   shift
+  local valid_levels=(
+    "INFO"
+    "ERROR"
+    "INTERNALERROR"
+  )
+  if ! is_elem_in_array "${level}" "${valid_levels[@]}"; then
+    log INTERNALERROR "Log level ${level} is not a valid level."
+    return 2
+  fi
   (
     # If any arguments are passed besides log level
     if [ "$#" -ge 1 ]; then
@@ -33,16 +65,15 @@ log() {
       # otherwise read log messages from standard input
       cat -
     fi
+    if [ "${level}" == "INTERNALERROR" ]; then
+      echo "Please report this: https://github.com/itzg/docker-mc-backup/issues"
+    fi
   ) | awk -v level="${level}" '{ printf("%s %s %s\n", strftime("%FT%T%z"), level, $0); fflush(); }'
-}
-
-find_old_backups() {
-  find "${DEST_DIR}" -maxdepth 1 -name "*.${backup_extension}" -mtime "+${PRUNE_BACKUPS_DAYS}" "${@}"
-}
+} >&2
 
 retry() {
   if [ "$#" -lt 3 ]; then
-    log ERROR "Wrong number of arguments passed to retry function"
+    log INTERNALERROR "Wrong number of arguments passed to retry function"
     return 1
   fi
 
@@ -78,8 +109,151 @@ retry() {
   return 2
 }
 
+is_function() {
+  if [ "${#}" -ne 1 ]; then
+    log INTERNALERROR "is_function expects 1 argument, received ${#}"
+  fi
+  name="${1}"
+  [ "$(type -t "${name}")" == "function" ]
+}
+
+call_if_function_exists() {
+  if [ "${#}" -lt 1 ]; then
+    log INTERNALERROR "call_if_function_exists expects at least 1 argument, received ${#}"
+    return 2
+  fi
+  function_name="${1}"
+  if is_function "${function_name}"; then
+    eval "${@}"
+  else
+    log INTERNALERROR "${function_name} is not a valid function!"
+    return 2
+  fi
+}
+
+#####################
+## specific method ##
+##    functions    ##
+#####################
+# Each function that corresponds to a name of a backup method
+# Should define following functions inside them
+# init() -> called before entering loop. Verify arguments, prepare for operations etc.
+# backup() -> create backup. It's guaranteed that all data is already flushed to disk.
+# prune() -> prune old backups. PRUNE_BACKUPS_DAYS is guaranteed to be positive.
+
+
+tar() {
+  _find_old_backups() {
+    find "${DEST_DIR}" -maxdepth 1 -name "*.${backup_extension}" -mtime "+${PRUNE_BACKUPS_DAYS}" "${@}"
+  }
+
+  init() {
+    mkdir -p "${DEST_DIR}"
+    readonly backup_extension="tgz"
+  }
+  backup() {
+    ts=$(date -u +"%Y%m%d-%H%M%S")
+    outFile="${DEST_DIR}/${BACKUP_NAME}-${ts}.${backup_extension}"
+    log INFO "Backing up content in ${SRC_DIR} to ${outFile}"
+    command tar "${excludes[@]}" -czf "${outFile}" -C "${SRC_DIR}" .
+    if [ "${LINK_LATEST^^}" == "TRUE" ]; then
+      ln -sf "${BACKUP_NAME}-${ts}.${backup_extension}" "${DEST_DIR}/latest.${backup_extension}"
+    fi
+  }
+  prune() {
+    if [ -n "$(_find_old_backups -print -quit)" ]; then
+      log INFO "Pruning backup files older than ${PRUNE_BACKUPS_DAYS} days"
+      _find_old_backups -print -delete | awk '{ printf "Removing %s\n", $0 }' | log INFO
+    fi
+  }
+  call_if_function_exists "${@}"
+}
+
+
+restic() {
+  _delete_old_backups() {
+    command restic forget --tag "${restic_tags_filter}" --keep-within "${PRUNE_BACKUPS_DAYS}d" "${@}"
+  }
+  _check() {
+      if ! output="$(command restic check 2>&1)"; then
+        log ERROR "Repository contains error! Aborting"
+        <<<"${output}" log ERROR
+        return 1
+      fi
+  }
+  init() {
+    if [ -z "${RESTIC_PASSWORD:-}" ] \
+        && [ -z "${RESTIC_PASSWORD_FILE:-}" ] \
+        && [ -z "${RESTIC_PASSWORD_COMMAND:-}" ]; then
+      log ERROR "At least one of" RESTIC_PASSWORD{,_FILE,_COMMAND} "needs to be set!"
+      return 1
+    fi
+    if [ -z "${RESTIC_REPOSITORY:-}" ]; then
+      log ERROR "RESTIC_REPOSITORY is not set!"
+      return 1
+    fi
+    # TODO: Not sure if this method is enough to determine if all needed configs are provided.
+    if output="$(command restic snapshots 2>&1 >/dev/null)"; then
+      log INFO "Repository already initialized"
+      _check
+    elif <<<"${output}" grep -q 'no such file or directory$'; then
+      log INFO "Initializing new restic repository..."
+      command restic init | log INFO
+    elif <<<"${output}" grep -q 'wrong password'; then
+      <<<"${output}" log ERROR
+      log ERROR "Wrong password provided to an existing repository?"
+      return 1
+    else
+      log INTERNALERROR "Unhandled restic repository state."
+      return 2
+    fi
+
+    # Used to construct tagging arguments and filters for snapshots
+    readonly restic_tags=(
+      "mc_backups"
+      "${BACKUP_NAME}"
+    )
+    # Arguments to use to tag the snapshots with
+    restic_tags_arguments=()
+    local tag
+    for tag in "${restic_tags[@]}"; do
+      restic_tags_arguments+=( --tag "${tag}" )
+    done
+    readonly restic_tags_arguments
+    # Used for filtering backups to only match ours
+    restic_tags_filter="$(IFS=,; echo "${restic_tags[*]}")"
+    readonly restic_tags_filter
+  }
+  backup() {
+    log INFO "Backing up content in ${SRC_DIR}"
+    command restic backup "${restic_tags_arguments[@]}" "${excludes[@]}" "${SRC_DIR}" | log INFO
+  }
+  prune() {
+    # We cannot use `grep -q` here - see https://github.com/restic/restic/issues/1466
+    if _delete_old_backups --dry-run | grep '^remove [[:digit:]]* snapshots:$' >/dev/null; then
+      log INFO "Forgetting snapshots older than ${PRUNE_BACKUPS_DAYS} days"
+      _delete_old_backups --prune | log INFO
+      _check | log INFO
+    fi
+  }
+  call_if_function_exists "${@}"
+}
+
+##########
+## main ##
+##########
+
 if [ -n "${INTERVAL_SEC:-}" ]; then
   log WARN 'INTERVAL_SEC is deprecated. Use BACKUP_INTERVAL instead'
+fi
+
+if [ ! -d "${SRC_DIR}" ]; then
+  log ERROR 'SRC_DIR does not point to an existing directory!'
+  exit 1
+fi
+
+if ! is_function "${BACKUP_METHOD}"; then
+  log ERROR "Invalid BACKUP_METHOD provided: ${BACKUP_METHOD}"
 fi
 
 # We unfortunately can't use a here-string, as it inserts new line at the end
@@ -90,6 +264,7 @@ for pattern in "${excludes_patterns[@]}"; do
   excludes+=(--exclude "${pattern}")
 done
 
+"${BACKUP_METHOD}" init
 
 log INFO "waiting initial delay of ${INITIAL_DELAY}..."
 # shellcheck disable=SC2086
@@ -101,7 +276,6 @@ retry 20 10s rcon-cli save-on
 
 
 while true; do
-  ts=$(date -u +"%Y%m%d-%H%M%S")
 
   if retry 5 10s rcon-cli save-off; then
     # No matter what we were doing, from now on if the script crashes
@@ -109,18 +283,9 @@ while true; do
     trap 'retry 5 5s rcon-cli save-on' EXIT
 
     retry 5 10s rcon-cli save-all
-    outFile="${DEST_DIR}/${BACKUP_NAME}-${ts}.${backup_extension}"
-    log INFO "backing up content in ${SRC_DIR} to ${outFile}"
+    retry 5 10s sync
 
-    # shellcheck disable=SC2086
-    if tar "${excludes[@]}" -czf "${outFile}" -C "${SRC_DIR}" .; then
-      log INFO "successfully backed up"
-      if [ "${LINK_LATEST^^}" == "TRUE" ]; then
-        ln -sf "${BACKUP_NAME}-${ts}.${backup_extension}" "${DEST_DIR}/latest.${backup_extension}"
-      fi
-    else
-      log ERROR "backup failed"
-    fi
+    "${BACKUP_METHOD}" backup
 
     retry 20 10s rcon-cli save-on
     # Remove our exit trap now
@@ -130,9 +295,8 @@ while true; do
     exit 1
   fi
 
-  if (( PRUNE_BACKUPS_DAYS > 0 )) && [ -n "$(find_old_backups -print -quit)" ]; then
-    log INFO "pruning backup files older than ${PRUNE_BACKUPS_DAYS} days"
-    find_old_backups -print -delete | log INFO
+  if (( PRUNE_BACKUPS_DAYS > 0 )); then
+    "${BACKUP_METHOD}" prune
   fi
 
   # If BACKUP_INTERVAL is not a valid number (i.e. 24h), we want to sleep.
---
+++ tests/test.simple.tar.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+set -x
+
+WORKDIR="$(readlink -m "$(dirname "${0}")")"
+
+cd "${WORKDIR}/.."
+
+TMP_DIR="$(mktemp --directory)"
+trap 'sudo rm -rf "${TMP_DIR}"' EXIT
+
+: "${SRC_DIR:=/tmp/source}"
+: "${DEST_DIR:=/tmp/dest}"
+: "${BACKUP_INTERVAL:=0}"
+: "${INITIAL_DELAY:=5s}"
+: "${EXCLUDES:='*.jar,exclude_dir'}"
+: "${RCON_PATH:=/usr/bin/rcon-cli}"
+: "${PRUNE_BACKUPS_DAYS:=3}"
+
+export LOCAL_SRC_DIR="${TMP_DIR}/${SRC_DIR}"
+export LOCAL_DEST_DIR="${TMP_DIR}/${DEST_DIR}"
+export TMP_DIR
+
+"${WORKDIR}/common.bootstrap.sh"
+
+export SRC_DIR
+export DEST_DIR
+export EXTRACT_DIR="${TMP_DIR}/extract"
+export BACKUP_INTERVAL
+export INITIAL_DELAY
+export EXCLUDES
+export RCON_PATH
+export PRUNE_BACKUPS_DAYS
+
+mkdir "${EXTRACT_DIR}"
+touch -d "$(( PRUNE_BACKUPS_DAYS + 2 )) days ago" "${LOCAL_DEST_DIR}/fake_backup_that_should_be_deleted.tgz"
+ls -al "${LOCAL_DEST_DIR}"
+
+timeout 50 docker run --rm \
+          --env SRC_DIR \
+          --env DEST_DIR \
+          --env BACKUP_INTERVAL \
+          --env INITIAL_DELAY \
+          --env EXCLUDES \
+          --env PRUNE_BACKUPS_DAYS \
+          --mount "type=bind,src=${LOCAL_SRC_DIR},dst=${SRC_DIR}" \
+          --mount "type=bind,src=${LOCAL_DEST_DIR},dst=${DEST_DIR}" \
+          --mount "type=bind,src=${TMP_DIR}/rcon-cli,dst=${RCON_PATH}" \
+          testimg
+
+tree "${LOCAL_DEST_DIR}"
+tar -xzf "${LOCAL_DEST_DIR}/"*.tgz -C "${EXTRACT_DIR}"
+tree "${EXTRACT_DIR}"
+[ -z "$(find "${EXTRACT_DIR}" -name "exclude_*" -print -quit)" ]
+[ 4 -eq "$(find "${EXTRACT_DIR}" -name "backup_me*" -print | wc -l)" ]
+
